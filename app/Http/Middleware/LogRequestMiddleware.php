868b87c3f09492a4655e7e035cd15c596682fbc6
Amairi <selsabilamairi@gmail.com>
2026-01-13 16:34:11 +0100

    Activité 5

---
+++ app/Http/Middleware/LogRequestMiddleware.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+
+class LogRequestMiddleware
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $userAgent = $request->header('User-Agent');
+        
+        Log::info('Navigateur utilisé : ' . $userAgent);
+        
+        return $next($request);
+    }
+}
---
+++ bootstrap/app.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\LogRequestMiddleware;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware): void {
+        // ← MODIFIER CETTE PARTIE
+        $middleware->append(LogRequestMiddleware::class);
+    })
+    ->withExceptions(function (Exceptions $exceptions): void {
+        //
+    })->create();
